11a89047bd0ed585d3db9d16b4d03f0c51cb54fa
azanniel <leo.azannielttt@gmail.com>
2023-07-17 22:01:26 -0400

    💥 add extra reducer

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,10 +1,9 @@
-import { useDispatch } from 'react-redux'
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { ChevronDown } from 'lucide-react'
 
 import { Lesson } from './Lesson'
 
-import { useAppSelector } from '../store'
+import { useAppDispatch, useAppSelector } from '../store'
 import { play } from '../store/slices/player'
 
 interface ModuleProps {
@@ -14,7 +13,7 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  const dispatch = useDispatch()
+  const dispatch = useAppDispatch()
 
   const { currentModuleIndex, currentLessonIndex } = useAppSelector((state) => {
     const { currentModuleIndex, currentLessonIndex } = state.player
